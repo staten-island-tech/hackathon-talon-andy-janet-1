2dd1442e19a4fa182e21935b77c134183d510bca
Andy-Zhang7223 <andyz127@nycstudents.net>
2024-11-22 09:29:49 -0500

    sdafdsa

---
+++ app2.py
@@ -0,0 +1,20 @@
+import pygame
+import random
+import time
+
+pygame.init()
+
+screen=pygame.display.set_mode(1000,500)
+
+#What variables does this game have?
+note_speed = 5
+note_speed = 5
+note_width = 50
+note_height = 30
+note_gap = 100
+score = 0
+misses = 0
+
+#Use colors
+TEAL = (0,150,155)
+BLUE = (0,50,205)
